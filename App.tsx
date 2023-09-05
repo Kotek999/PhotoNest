481906682d7fd2a06053f4f74a3e958adba7e4e9
Filip <filiptankiewicz04@gmail.com>
2023-09-05 13:54:30 +0200

    redux added

--- App.tsx
+++ App.tsx
@@ -1,5 +1,11 @@
+import { Provider } from "react-redux";
+import { store } from "./store";
 import { Root } from "./root";
 
 export default function App() {
-  return <Root />;
+  return (
+    <Provider store={store}>
+      <Root />
+    </Provider>
+  );
 }
--- src/Screens/Test/index.tsx
+++ src/Screens/Test/index.tsx
@@ -10,15 +10,19 @@ import {
 } from "react-native";
 import { NavigationScreenProps } from "../../../rootTypeList";
 import { SCREEN } from "../../../routes";
+import { useSelector, useDispatch } from "react-redux";
+import { counterSlice } from "../../redux/counter/action";
 
 export const Test = ({ navigation }: NavigationScreenProps<SCREEN.Test>) => {
+  const counter = useSelector((state: { counter: number }) => state.counter);
+  const dispatch = useDispatch();
   const source: ImageSourcePropType = require("../../assets/bg.png");
 
   return (
     <SafeAreaView style={styles.container}>
       <ImageBackground source={source} resizeMode="cover" style={styles.image}>
         <Text style={{ textAlign: "center", fontSize: 20 }}>
-          This is Test Screen
+          This is Test Screen ---- {counter}
         </Text>
         <View
           style={{
@@ -31,6 +35,14 @@ export const Test = ({ navigation }: NavigationScreenProps<SCREEN.Test>) => {
           <Button
             title="Back to Login"
             onPress={() => navigation.navigate(SCREEN.Login)}
+          />
+          <Button
+            title="Increase"
+            onPress={() => dispatch(counterSlice.actions.increment())}
+          />
+          <Button
+            title="decrease"
+            onPress={() => dispatch(counterSlice.actions.decrement())}
           />
         </View>
       </ImageBackground>
